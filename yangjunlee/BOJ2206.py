7832a2ed3a2a24e41ee287a2651772d5c36aea97
YANGJUN LEE <dldidwns123@snu.ac.kr>
2023-08-06 18:20:19 +0900

    Update BOJ2206.py

--- yangjunlee/BOJ2206.py
+++ yangjunlee/BOJ2206.py
@@ -12,6 +12,7 @@ def bfs():
     while queue:
         x,y,z=queue.popleft()
         for i in range(4):
+            if x==n-1 and y==m-1: return visited[x][y][z]
             xp, yp=x+dx[i], y+dy[i]
             if xp<0 or xp>=n or yp<0 or yp>=m:continue
             if box[xp][yp]==0 and visited[xp][yp][z]==0:
@@ -20,8 +21,7 @@ def bfs():
             if z==0 and box[xp][yp]==1 and visited[xp][yp][z+1]==0:
                 visited[xp][yp][z+1]=visited[x][y][z]+1
                 queue.append([xp, yp, z+1])
+    return -1
 
 
-bfs()
-print(visited[n-1][m-1][0] if visited[n-1][m-1][0]!=0 else visited[n-1][m-1][1] if visited[n-1][m-1][1]!=0 else -1)
-
+print(bfs())

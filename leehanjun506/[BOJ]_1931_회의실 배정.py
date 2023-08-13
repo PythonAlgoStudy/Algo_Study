3b0decd7164b395f4adab2ac3f680c1207c9bf20
leehanjun506 <sis03017@naver.com>
2023-08-13 16:49:21 +0900

    feat : [BOJ]_1931_회의실 배정

--- leehanjun506/[BOJ]_1931_회의실 배정.py
+++ leehanjun506/[BOJ]_1931_회의실 배정.py
@@ -1,6 +1,6 @@
 n = int(input()) # 회의의 수
 time = [list(map(int,input().split())) for _ in range(n)]
-# time.sort(key = lambda x: x[1])
+# time.sort(key = lambda x: x[1]) 
 # 시작하는 시간도 정렬 해줘야 한다. 시작하자마자 끝나는 경우 ex ([4,4],[4,4],[1,4]) 
 time.sort(key = lambda x : (x[1],x[0]))
 
